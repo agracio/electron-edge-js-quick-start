d81b15151555cfa85fb0d15dcadd76a52f859b2e
agracio <antonas.graciovas@outlook.com>
2018-06-13 23:19:36 +0100

    updating quick start

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1052, height: 864});
+  mainWindow = new BrowserWindow({width: 1052, height: 600});
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html?version=${version}`);

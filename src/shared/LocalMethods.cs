9001d3a8ddc6714601b1c852a7f290f9fb2e2d22
agracio <antonas.graciovas@outlook.com>
2024-06-07 13:01:54 +0100

    updating packages

--- src/shared/LocalMethods.cs
+++ src/shared/LocalMethods.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace QuickStart
 {
